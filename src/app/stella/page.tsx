f408bc4ae8136615205c793cc047d275c8aec87f
Lucas ROdrigues <llucasy@hotmail.com>
2024-10-08 22:04:51 -0300

    feat: display ui with subjects

--- src/app/stella/page.tsx
+++ src/app/stella/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center text-center pt-12">
       <h1 className="text-3xl font-bold underline mb-8">Quiz Lucas</h1>
-      {quizState.gameStage === 'Start' && <Welcome name="Stella" />}
+      {quizState.gameStage === 'Start' && <Welcome name="stella" />}
       {quizState.gameStage === 'Playing' && <Question />}
       {quizState.gameStage === 'End' && <GameOver />}
     </main>
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -3,23 +3,47 @@
 import Image from 'next/image'
 
 import quizImg from '@/img/quiz.svg'
-import { Dispatch, useContext } from 'react'
+import { Dispatch, useContext, useEffect, useRef, useState } from 'react'
 import { QuizContext } from '@/contexts/Quiz'
 
-const Welcome = ({name}: { name: string }) => {
+const Welcome = ({ name }: { name: string }) => {
   const [quizState, dispatch] = useContext<any>(QuizContext)
+  const [subjects, setSubjects] = useState<[{ subject: string }]>([{ subject: '' }])
+  const nameRef = useRef(name)
+
+  useEffect(() => {
+    ;(async () => {
+      const response = await fetch(`api/subject/${nameRef.current}`)
+      const data: [{ subject: string }] = await response.json()
+      return data
+    })().then((data) => setSubjects(data))
+  }, [])
 
   return (
     <div className="flex flex-col justify-center items-center max-w-[500px]">
-      <h2 className="text-4xl font-bold mb-4">Seja bem-vinda, <span className='text-purple-500'>{name}</span>!</h2>
+      <h2 className="text-4xl font-bold mb-4">
+        Seja bem-vinda, <span className="text-purple-500 capitalize">{name}</span>!
+      </h2>
       <p className="mb-4 text-[#8435de]">Clique no botão abaixo para começar:</p>
-      <button
+      <div className="flex gap-6">
+        {subjects.map((subject) => (
+          <button
+            key={subject.subject}
+            onClick={() => dispatch({ type: 'CHANGE_STATE' })}
+            type="button"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            {subject.subject}
+          </button>
+        ))}
+      </div>
+      {/* <button
         onClick={() => dispatch({ type: 'CHANGE_STATE' })}
         type="button"
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         Iniciar
-      </button>
+      </button> */}
       <Image className="mt-8" src={quizImg} alt="Inicio do Quiz" />
     </div>
   )
